a1c794f741a455a5ac61ddb6b084ae74e477b580
Tuwan <2017431971@qq.com>
2022-11-10 17:18:09 +0800

    hot-fix commit2

--- src/main/java/com/jch/git/GitTest.java
+++ src/main/java/com/jch/git/GitTest.java
@@ -5,5 +5,8 @@ public class GitTest {
         System.out.println("11");
         System.out.println("22");
         System.out.println("33");
+        System.out.println("44");
+        System.out.println("55");
+        System.out.println("66");
     }
 }
